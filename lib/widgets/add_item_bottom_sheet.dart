f6ebd26f4fe35cdb839cf3547bfda4483bd76358
balvinderz <balvindersi2@gmail.com>
2020-01-10 21:33:29 +0530

    added date

--- lib/widgets/add_item_bottom_sheet.dart
+++ lib/widgets/add_item_bottom_sheet.dart
@@ -10,16 +10,81 @@ class AddItemBottomSheet extends StatefulWidget {
 class _AddItemBottomSheetState extends State<AddItemBottomSheet> {
   String dropDownValue = "soja";
   String typeDropDownValue = "Debited";
-  TextEditingController itemNameController,costController;
+  TextEditingController itemNameController, costController, dateController;
+  DateTime _date = DateTime.now();
+  String getFormattedDate(){
+    return "${_date.day} ${getMonth(_date.month)} ${_date.year}";
 
+  }
   @override
   void initState() {
-      itemNameController = TextEditingController();
-      costController = TextEditingController();
+    itemNameController = TextEditingController();
+    costController = TextEditingController();
+    dateController = TextEditingController();
+  }
+
+  String getMonth(int dateMonth) {
+    var month;
+    switch (dateMonth) {
+      case 1:
+        month = "January";
+        break;
+      case 2:
+        month = "February";
+        break;
+      case 3:
+        month = "March";
+        break;
+      case 4:
+        month = "April";
+        break;
+      case 5:
+        month = "May";
+        break;
+      case 6:
+        month = "June";
+        break;
+      case 7:
+        month = "July";
+        break;
+      case 8:
+        month = "August";
+        break;
+      case 9:
+        month = "September";
+        break;
+      case 10:
+        month = "October";
+        break;
+      case 11:
+        month = "November";
+        break;
+      case 12:
+        month = "December";
+        break;
+    }
+    return month;
+  }
+
+  Future<Null> selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: _date,
+        firstDate: DateTime(2020),
+        lastDate: _date);
+    if (picked != null && picked != _date) {
+      setState(() {
+        _date = picked;
+
+        //print(_date.toIso8601String());
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    dateController.text = getFormattedDate();
+
     // TODO: implement build
     return Container(
       padding: EdgeInsets.all(20),
@@ -42,8 +107,18 @@ class _AddItemBottomSheetState extends State<AddItemBottomSheet> {
             decoration: InputDecoration(hintText: "Item Name"),
           ),
           TextField(
+              readOnly: true,
+              textAlign: TextAlign.center,
+
+              controller: dateController,
+              decoration: InputDecoration(hintText: "date"),
+              onTap: () async {
+                print("soja");
+                await selectDate(context);
+              }),
+          TextField(
             controller: costController,
-            keyboardType: TextInputType.number,
+            keyboardType: TextInputType.datetime,
             textAlign: TextAlign.center,
             autofocus: false,
             decoration: InputDecoration(hintText: "Cost"),
@@ -55,10 +130,9 @@ class _AddItemBottomSheetState extends State<AddItemBottomSheet> {
             items: <String>['soja', 'B', 'C', 'D'].map((String value) {
               return new DropdownMenuItem<String>(
                 value: value,
-                child:  Center(child: Text(value)),
+                child: Center(child: Text(value)),
               );
             }).toList(),
-
             onChanged: (newValue) {
               setState(() {
                 dropDownValue = newValue;
@@ -72,7 +146,7 @@ class _AddItemBottomSheetState extends State<AddItemBottomSheet> {
             items: <String>['Debited', 'Credited'].map((String value) {
               return new DropdownMenuItem<String>(
                 value: value,
-                child:  Center(child: Text(value)),
+                child: Center(child: Text(value)),
               );
             }).toList(),
 
@@ -88,16 +162,28 @@ class _AddItemBottomSheetState extends State<AddItemBottomSheet> {
               splashColor: Colors.blue,
               onPressed: () async {
                 String itemName = itemNameController.text;
+                if(itemName=="")
+                  return ;
                 String cost = costController.text;
+                if(cost =="")
+                  return ;
                 print(itemName);
                 print(cost);
                 print("adding item");
-                Item item = Item(itemName: itemName,cost: cost,type: typeDropDownValue,category: dropDownValue,date: "28 December 2019");
-               var res =  await BachatDBProvider.db.insertItem(item);
-               print(res) ;
-               Navigator.pop(context);
+                Item item = Item(
+                    itemName: itemName,
+                    cost: cost,
+                    type: typeDropDownValue,
+                    category: dropDownValue,
+                    date: getFormattedDate());
+                var res = await BachatDBProvider.db.insertItem(item);
+                print(res);
+                Navigator.pop(context);
               },
-              child: Text("Add Item",style: TextStyle(color: Colors.white),),
+              child: Text(
+                "Add Item",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           )
         ],
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -2,36 +2,77 @@ import 'package:bachat/screens/bachat_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MyDrawer extends StatelessWidget
-{
+const whiteTextStyle = TextStyle(
+    color: Colors.white
+    );
+
+class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return  Drawer(
-      child: new ListView(
-        children: <Widget>[
-          new ListTile(
-            trailing: new Icon(Icons.cancel),
-            onTap:() => Navigator.of(context).pop(),
-          ),
-          new ListTile(
-            title: new Text("Homepage"),
-            trailing: new Icon(Icons.home),
-            onTap:()=>Navigator.of(context).push(new MaterialPageRoute(builder:(BuildContext context)=> BachatPage())),
-          ),
-          new ListTile(
-            title: new Text("Categories"),
-            trailing: new Icon(Icons.category),
-          ),
-          new ListTile(
-            title: new Text("Cards"),
-            trailing: new Icon(Icons.credit_card),
-          ),
-          new ListTile(
-            title: new Text("Calender"),
-            trailing: new Icon(Icons.calendar_today),
-          )
-        ],
+    return Drawer(
+      child: Container(
+        color: Color(0xFF017AAD),
+        child: new Column(
+          children: <Widget>[
+            new ListTile(
+              trailing: new Icon(
+                Icons.cancel,
+                color: Colors.white,
+              ),
+              onTap: () => Navigator.of(context).pop(),
+            ),
+            Divider(
+              color: Colors.white,
+            ),
+            ListTile(
+              title: new Text("Homepage", style: whiteTextStyle,),
+              leading: new Icon(Icons.home, color: Colors.white),
+              onTap: () =>
+                  Navigator.of(context).push(new MaterialPageRoute(
+                      builder: (BuildContext context) => BachatPage())),
+            ),
+            Divider(
+              color: Colors.white,
+            ),
+            ListTile(
+              title: new Text("Categories", style: whiteTextStyle,),
+              leading: new Icon(Icons.category, color: Colors.white),
+            ),
+            Divider(
+              color: Colors.white,
+            ),
+            ListTile(
+              title: new Text("Cards", style: whiteTextStyle,),
+              leading: new Icon(Icons.credit_card, color: Colors.white),
+            ),
+            Divider(
+              color: Colors.white,
+            ),
+            ListTile(
+              title: new Text("Calender", style: whiteTextStyle,),
+              leading: new Icon(Icons.calendar_today, color: Colors.white),
+            ),
+            Divider(
+              color: Colors.white,
+            ),
+            ListTile(
+              title: new Text("-", style: whiteTextStyle,),
+              leading: new Icon(Icons.home, color: Colors.white),
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: IconButton(
+                  onPressed: (){
+
+                  },
+                  icon: Icon(Icons.settings,color: Colors.white,),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
